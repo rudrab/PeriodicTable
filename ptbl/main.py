8b73d7850b094e2fa018403807be7ef058c65bae
Rudra Banerjee <rudra@phy-roddur.physics.uu.se>
2016-12-26 10:46:47 +0100

    New atoms names updated (at per with 30th Nov IUPAC announcment.)

--- ptbl/main.py
+++ ptbl/main.py
@@ -19,7 +19,7 @@ col = [['H', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'He
         'Hf', 'Ta', 'W', 'Re', 'Os', 'It', 'Pt', 'Au', 'Hg', 'Ti', 'Pb', 'Bi',  'Po', 'At', 'Rn'],
        ['Fr', 'Ra', 'Ac',
         "Th", "Pa", "U", "Np", "Pu", "Am", "Cu", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr",
-        "Rf", "Db", "Sg", "Bh", 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'UUt', 'Fi', 'UUp', 'Lv', 'UUs', 'UUo']
+        "Rf", "Db", "Sg", "Bh", 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
        ]
 
 class GridWindow(Gtk.ApplicationWindow):
